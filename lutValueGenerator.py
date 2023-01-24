128bfccedbc7b1fd309734e0c8262593c8aa1c89
fereshtehbaradaran <bara.frsht@gmail.com>
2023-01-24 23:57:30 +0330

    lut value generator added

---
+++ lutValueGenerator.py
@@ -0,0 +1,38 @@
+fileName = "LUTValues.txt"
+numberOfBits = 8  # 256 = 2 ^ 8
+LUTSize = 2 ** numberOfBits
+
+func = lambda x: x**2 - x**3
+
+
+def binaryToFractional(binaryStr):
+    return int(binaryStr, 2) / 2 ** len(binaryStr)
+
+
+def floatingPartTobinary(number, size):
+    fractionalPart = number - int(number)
+    result = ""
+    
+    for i in range(size):
+        fractionalPart *= 2
+        
+        if (int(fractionalPart) == 1):
+            fractionalPart -= 1
+            result += "1"
+        else:
+            result += "0"
+    
+    return result
+
+
+if __name__ == "__main__":
+    dataFile = open(fileName, 'w')
+
+    for i in range(LUTSize):
+        xBinaryStr = format(i, "08b")
+        x = binaryToFractional(xBinaryStr)
+        y = func(x)
+        yBinaryStr = 9
+        dataFile.write(yBinaryStr + "\n")
+
+    dataFile.close()    
