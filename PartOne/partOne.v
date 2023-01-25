ec5bfc700c5799a64d3d6a11c844c09fc5bcd73c
Fereshteh Baradaran <61563792+fereshtehbaradaran@users.noreply.github.com>
2023-01-25 09:27:23 +0330

    Update partOne.v

--- PartOne/partOne.v
+++ PartOne/partOne.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    00:22:18 01/25/2023 
-// Design Name: 
-// Module Name:    partOne 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module partOne(
     input [7:0] x,
     output reg [7:0] y
